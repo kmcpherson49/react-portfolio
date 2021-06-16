0db37bc19042d264fbeed896488ba336b3021f70
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-06-15 21:16:00 -0700

    add nav logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav/nav';
 import About from './components/About/about';
 import Contact from './components/Contact/contact';
@@ -8,14 +8,37 @@ import Contact from './components/Contact/contact';
 //import Resume from '/components/Resume';
 
 function App() {
+  const[tabs] = useState([
+    {name: 'contact',
+  description: "Let's get in touch"
+  },
+  {name: 'portfolio',
+  description: "A collection of my work"
+  },
+  {name: 'resume',
+  description: "Take a look at the things I've done"
+  },
+  ]);
+
+  const [currentTab, setCurrentTab] = useState(tabs[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+      tabs={tabs}
+      setCurrentTab={setCurrentTab}
+      currentTab={currentTab}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
+      ></Nav>
       <main>
       <About></About>
-      <Contact></Contact>
       {/* <Portfolio></Portfolio>
       <Resume></Resume> */}
+      <Contact></Contact>
+    
       </main>
     </div>
   );
--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import coverImage from '../../assets/cover/portfolio-header.jpg';
 
-function Nav() {
+function Nav(props) {
+  const {
+    tabs = [],
+    setCurrentTab,
+    contactSelected,
+    currentTab,
+    setContactSelected,
+  } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentTab.name);
+  }, [currentTab]);
+
   return (
-    <header>
-    <h1>
+    <header className="flex-row px-1">
+    {/* <h1>
       <a href="/">Krista McPherson's Portfolio</a>
-    </h1>
+    </h1> */}
+    <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
     <nav>
-      <ul>
-        <li>
-          <a href="#about">About Me</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#resume">Resume</a>
-        </li>
-        <li>
-          <a href="#contact">Contact Me</a>
-        </li>
-      </ul>
-    </nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+              About me
+            </a>
+          </li>
+          {tabs.map((tab) => (
+            <li
+              className={`mx-1 ${
+                currentTab.name === tab.name && !contactSelected && 'navActive'
+                }`}
+              key={tab.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentTab(tab);
+                  setContactSelected(false);
+                }}
+              >
+                {capitalizeFirstLetter(tab.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
   </header>
   );
 }
